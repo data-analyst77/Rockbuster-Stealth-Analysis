9192e5d73e9a8fc7abe65b688c74bbe2fc6e559c
DA77 <195586355+data-analyst77@users.noreply.github.com>
2025-03-07 16:44:05 +0100

    Update 02_Cleaning_Data_PostgreSQL.sql

--- 02_Queries/02_Cleaning_Data_PostgreSQL.sql
+++ 02_Queries/02_Cleaning_Data_PostgreSQL.sql
@@ -1,1 +1,12 @@
+--Check for full duplicates based on 4 columns in film
+
+SELECT title, 
+       release_year, 
+       language_id, 
+       rental_duration, 
+       COUNT(*) AS duplicate_count
+FROM film
+GROUP BY title, release_year, language_id, rental_duration
+HAVING COUNT(*) > 1;
+--No duplicates in film table found.  
 
