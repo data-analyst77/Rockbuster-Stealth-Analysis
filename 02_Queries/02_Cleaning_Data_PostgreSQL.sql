59ade9fdaf1fe45ca30c5012a6e535b8051ea35d
DA77 <195586355+data-analyst77@users.noreply.github.com>
2025-03-08 19:32:01 +0100

    Update 02_Cleaning_Data_PostgreSQL.sql

--- 02_Queries/02_Cleaning_Data_PostgreSQL.sql
+++ 02_Queries/02_Cleaning_Data_PostgreSQL.sql
@@ -1,5 +1,4 @@
 --Check for full duplicates based on 4 columns in film
-
 SELECT title, 
        release_year, 
        language_id, 
@@ -9,4 +8,42 @@ FROM film
 GROUP BY title, release_year, language_id, rental_duration
 HAVING COUNT(*) > 1;
 --No duplicates in film table found.  
+
+--Clean for duplicate data (if there were duplicates).
+--Delete redundant rows. 
+DELETE FROM film
+WHERE film_id NOT IN (
+    SELECT MIN(film_id)
+    FROM film
+    GROUP BY title, release_year, language_id, rental_duration
+);
+
+--Add a unique constraint to prevent future duplicates. 
+ALTER TABLE film 
+ADD CONSTRAINT unique_film UNIQUE (title, release_year, language_id, rental_duration);
+
+--Add constraint to prevent NULL entries. 
+ALTER TABLE film 
+ALTER COLUMN title SET NOT NULL;
+
+--Check for non-uniform titles (case-insensitive). 
+SELECT LOWER(title) AS normalized_title, 
+       COUNT(*) AS non_uniform_count
+FROM film
+GROUP BY LOWER(title)
+HAVING COUNT(*) > 1;
+-- No non-uniform entries for title in film table. 
+
+--Cleaning. Standardize names or titles with consistent casing.
+UPDATE film
+SET title = INITCAP(title); -- Capitalizes the first letter of each word.
+
+--Cleaning. Replace missing values with ‘unkown’.
+UPDATE film
+SET title = 'Unknown'
+WHERE title IS NULL;
+
+
+
+
 
