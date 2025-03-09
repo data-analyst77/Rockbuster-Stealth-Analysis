2c9361deaf14ef7217f0ac0a4973c4ff0318ebe7
DA77 <195586355+data-analyst77@users.noreply.github.com>
2025-03-09 12:01:28 +0100

    Update 03_Join_Data_PostgreSQL.sql

--- 02_Queries/03_Join_Data_PostgreSQL.sql
+++ 02_Queries/03_Join_Data_PostgreSQL.sql
@@ -1,27 +1,38 @@
 # Joining Tables of Data 
 
--- 1.	Write a query to find the top 10 countries for Rockbuster in terms of customer numbers. (Tip: you’ll have to use GROUP BY and ORDER BY, both of which follow the join.)  
+-- 1. Write a query to find the top 10 countries for Rockbuster in terms of customer numbers. 
+-- Tip: You’ll have to use GROUP BY and ORDER BY, both of which follow the join.  
 
---Top 10 countries by customer number
-SELECT 
-D.country, 
-COUNT (A.customer_id) AS customer_count
+## Assumptions:
+-- 1. Table A = customer
+-- 2. Table B = address
+-- 3. Table C = city
+-- 4. Table D = country
+
+-- Top 10 countries by customer number
+SELECT D.country, COUNT(A.customer_id) AS customer_count
 FROM customer A
-INNER JOIN address B ON A.address_id = B.address_id
-INNER JOIN city C ON B.city_id = C.city_id
+
+-- Joining customer to address (Assuming each customer has one address)
+INNER JOIN address B ON A.address_id = B.address_id 
+
+-- Joining address to city (Each address belongs to a city)
+INNER JOIN city C ON B.city_id = C.city_id 
+
+-- Joining city to country (Each city belongs to a country)
 INNER JOIN country D ON C.country_ID = D.country_ID 
+
+-- Grouping by country to count customers per country
 GROUP BY D.country
-ORDER BY customer_count DESC 
+
+-- Ordering in descending order to show countries with the most customers first
+ORDER BY customer_count DESC
+
+-- Limiting to the top 10 countries
 LIMIT 10;
 
 
-## Assumptions
-1.	Table A=customer
-2.	Table B=address
-3.	Table C=city
-4.	Table D=country
-
-## 2. Write a query to identify the top 10 cities that fall within the top 10 countries you identified in step 1. 
+-- 2. Write a query to identify the top 10 cities that fall within the top 10 countries you identified in step 1. 
 
 SELECT 
 D.country, 
