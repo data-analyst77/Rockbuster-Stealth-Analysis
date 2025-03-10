bff827b9a77ad4a9d12dbde3bd406c736258f39d
DA77 <195586355+data-analyst77@users.noreply.github.com>
2025-03-10 10:48:46 +0100

    Update 03_Join_Data_PostgreSQL.sql

--- 02_Queries/03_Join_Data_PostgreSQL.sql
+++ 02_Queries/03_Join_Data_PostgreSQL.sql
@@ -32,27 +32,3 @@ ORDER BY customer_count DESC
 LIMIT 10;
 
 
--- 2. Write a query to identify the top 10 cities that fall within the top 10 countries you identified in step 1. 
-
-SELECT 
-D.country, 
-C.city, 
-COUNT (A.customer_id) AS customer_count
-FROM customer A
-INNER JOIN address B ON A.address_id = B.address_id
-INNER JOIN city C ON B.city_id = C.city_id
-INNER JOIN country D ON C.country_ID = D.country_ID
-GROUP BY D.country, C.city
-
-HAVING D.country IN ( 
-SELECT D.country 
-FROM customer A 
-INNER JOIN address B ON A.address_id =B.address_id 
-INNER JOIN city C ON B.city_id = C.city_id 
-INNER JOIN country D ON C.country_id = D.country_id
-GROUP BY D.country 
-ORDER BY COUNT(A.customer_id) DESC LIMIT 10 )
-
-ORDER BY customer_count DESC 
-LIMIT 10;
-
